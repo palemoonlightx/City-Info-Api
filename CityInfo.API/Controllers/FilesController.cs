26dcdddbf1da5788dd4a9b6fcd7050c108aa9a8e
Alin <alin@gmail.com>
2023-10-25 15:36:37 +0200

    Updated code

---
+++ CityInfo.API/Controllers/FilesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace CityInfo.API.Controllers {
+
+    [ApiController]
+    [Route("api/files")]
+    public class FilesController : ControllerBase {
+
+        private readonly FileExtensionContentTypeProvider _fct;
+
+        public FilesController(FileExtensionContentTypeProvider fct) {
+            _fct = fct ?? throw new ArgumentNullException(nameof(fct));
+        }
+
+        [HttpGet("{fileId}")]
+        public ActionResult GetFile(string fileId) {
+
+
+            var pathToFile = "image.jpg";
+            if (!System.IO.File.Exists(pathToFile)) {
+                return NotFound();
+            }
+
+            if (!_fct.TryGetContentType(pathToFile, out var contentType)) {
+                contentType = "application/octet-stream";
+            }
+
+            var bytes = System.IO.File.ReadAllBytes(pathToFile);
+
+            return File(bytes, contentType, Path.GetFileName(pathToFile));
+
+        }
+    }
+}
+ 
--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -1,19 +1,25 @@
+using Microsoft.AspNetCore.StaticFiles;
+
 namespace CityInfo.API {
     public class Program {
         public static void Main(string[] args) {
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddControllers();
-              
+            builder.Services.AddControllers(options => {
+                options.ReturnHttpNotAcceptable = true;
+            }).AddXmlDataContractSerializerFormatters();
+
             // Injecting Swagger
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddSingleton<FileExtensionContentTypeProvider>();
+
 
             var app = builder.Build();
 
             // Adding swagger middleware
-            if(app.Environment.IsDevelopment()) {
+            if (app.Environment.IsDevelopment()) {
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
